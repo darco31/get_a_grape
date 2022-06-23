dc0f336bcd2257a38d08615aac52c2c4167aafda
Stephen <sdarcy29@hotmail.com>
2022-06-23 08:27:14 +0000

    Changed the field in products to name from producer and re ran migrations. Also I updated the products json file to include a URL but have left this blank for now2

--- products/models.py
+++ products/models.py
@@ -24,7 +24,7 @@ class Product(models.Model):
     """
 
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
-    producer = models.CharField(max_length=254)
+    name = models.CharField(max_length=254)
     location = models.CharField(max_length=254)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
@@ -34,4 +34,4 @@ class Product(models.Model):
     image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
-        return self.producer
+        return self.name
