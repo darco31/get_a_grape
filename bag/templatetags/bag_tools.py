a92795acfed42238d9cead5ef8b9330b84d8b0e8
Stephen <sdarcy29@hotmail.com>
2022-07-01 19:50:23 +0000

    Added ,essages to the templates to display a message to user when adding items to bag

--- bag/templatetags/bag_tools.py
+++ bag/templatetags/bag_tools.py
@@ -1,11 +1,15 @@
+"""
+Import the templates from django
+"""
 from django import template
 
 register = template.Library()
 
+
 @register.filter(name='calculate_subtotal')
 def calculate_subtotal(price, quantity):
     """
-    Tag to display the correct subtotal in the 
+    Tag to display the correct subtotal in the
     shopping bag
     """
     return price * quantity
