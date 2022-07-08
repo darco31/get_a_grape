6458847b8ec85d81aad81f0a48aa7d4c7bc58653
Stephen <sdarcy29@hotmail.com>
2022-07-08 19:34:13 +0000

    Add views, templates and urls for the profile app

---
+++ profiles/urls.py
@@ -0,0 +1,7 @@
+""" Profile urls """
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.profile, name='profile')
+]
--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,11 @@
+"""User profile view imports"""
 from django.shortcuts import render
 
-# Create your views here.
+
+def profile(request):
+    """Display the user profile"""
+
+    template = 'profiles/profile.html'
+    context = {}
+
+    return render(request, template, context)
