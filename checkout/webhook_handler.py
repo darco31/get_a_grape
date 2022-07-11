3a432877041e379873aa45f89c7ea75d92d927e6
Stephen <sdarcy29@hotmail.com>
2022-07-11 19:55:07 +0000

    Add the ability to send confirmation emials to customers and tested this successfully in the terminal

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -3,9 +3,9 @@ import time
 import json
 from django.http import HttpResponse
 from profiles.models import UserProfile
-# from django.core.mail import send_mail
-# from django.template.loader import render_to_string
-# from django.conf import settings
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.conf import settings
 from products.models import Product
 from .models import Order, OrderLineItem
 
@@ -16,6 +16,19 @@ class StripeWH_Handler:
     """
     def __init__(self, request):
         self.request = request
+
+    def send_confirmation_email(self, order):
+        """ Send user a mail on confirmation"""
+        cust_email = order.email
+        subject = render_to_string(
+            'checkout/confirmation_emails/confirmation_email_subject.txt',
+            {'order': order})
+        body = render_to_string(
+            'checkout/confirmation_emails/confirmation_email_body.txt',
+            {'order': order, 'contact_email': settings.DEFAULT_FROM_EMAIL})
+
+        send_mail(subject, body, settings.DEFAULT_FROM_EMAIL, [cust_email])
+
 
     def handle_event(self, event):
         """
@@ -83,6 +96,7 @@ class StripeWH_Handler:
                 attempt += 1
                 time.sleep(1)
         if order_exists:
+            self.send_confirmation_email(order)
             return HttpResponse(
                 content=f'Webhook received: {event["type"]} | SUCCESS: \
                     Verified order already in database',
@@ -119,6 +133,7 @@ class StripeWH_Handler:
                 return HttpResponse(
                     content=f'Webhook received: {event["type"]} | ERROR: {e}',
                     status=500)
+        self.send_confirmation_email(order)
         return HttpResponse(
             content=f'Webhook received: {event["type"]} | SUCCESS: Created \
                 order in webhook',
