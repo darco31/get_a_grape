b9fc2bc6ffc30c1083651859a84ff8a2590f9ee9
Stephen <sdarcy29@hotmail.com>
2022-07-12 15:18:12 +0000

    Addview, url and buttons for editing and deleting products for admin only

--- products/views.py
+++ products/views.py
@@ -86,9 +86,9 @@ def add_product(request):
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            product = form.save()
             messages.success(request, 'You have added a new product')
-            return redirect(reverse('add_product'))
+            return redirect(reverse('product_detail', args=[product.id]))
         else:
             messages.error(request, 'Failed to add product')
     else:
@@ -127,3 +127,13 @@ def edit_product(request, product_id):
     }
 
     return render(request, template, context)
+
+
+def delete_product(request, product_id):
+    """
+    Delete a product
+    """
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, f'You have deleted {product.name}')
+    return redirect(reverse('products'))
