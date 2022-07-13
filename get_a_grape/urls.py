280b53a3e980159d6afb23cb912ff69ec19071c9
Stephen <sdarcy29@hotmail.com>
2022-07-13 16:01:10 +0000

    Add first error page to test 404.html

--- get_a_grape/urls.py
+++ get_a_grape/urls.py
@@ -16,4 +16,4 @@ urlpatterns = [
     path('profile/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-handler404 = 'boutique_ado.views.handler404'
+handler404 = 'get_a_grape.views.handler404'
