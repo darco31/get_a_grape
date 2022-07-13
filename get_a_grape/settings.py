ac68321ff044785720b26c51b4c21233c1b594df
Stephen <sdarcy29@hotmail.com>
2022-07-13 09:19:17 +0000

    Removed secret key and set the DEBUG

--- get_a_grape/settings.py
+++ get_a_grape/settings.py
@@ -10,10 +10,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-sjj#d87b1)!3do07s-$t$g0gj&(blhpompx$^=&rjjhob@_2)^'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['get-a-grape.herokuapp.com', 'localhost']
 
