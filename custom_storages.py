cfc16feb6d0dd41a507f86d3380f376d39514ec5
Stephen <sdarcy29@hotmail.com>
2022-07-13 12:41:57 +0000

    Add new deployment parameters to settings.py as well as set up storages for AWS

---
+++ custom_storages.py
@@ -0,0 +1,19 @@
+"""
+Imports for storage files
+"""
+from django.conf import settings
+from storages.backends.s3boto3 import S3Boto3Storage
+
+
+class StaticStorage(S3Boto3Storage):
+    """
+    Storage locations for static files
+    """
+    location = settings.STATICFILES_LOCATION
+
+
+class MediaStorage(S3Boto3Storage):
+    """
+    Storage locations for static files
+    """
+    location = settings.MEDIAFILES_LOCATION
