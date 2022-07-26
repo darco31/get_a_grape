158a0f602708d393b175cd1614d3a376cf00343f
Stephen <sdarcy29@hotmail.com>
2022-07-26 16:33:41 +0000

    Test

--- get_a_grape/settings.py
+++ get_a_grape/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', ' ')
 
 # DEBUG = 'DEVELOPMENT' in os.environ
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['get-a-grape.herokuapp.com', 'localhost']
 
